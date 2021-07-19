ce833ca8953c6f8a7802777e864fcdb94e527b9e
odas <zwtao106@gmail.com>
2021-07-20 02:49:11 +1000

    c命名空间 p命名空间

--- spring-04-di/src/test/java/MyTest.java
+++ spring-04-di/src/test/java/MyTest.java
@@ -1,4 +1,6 @@
 import com.odas.pojo.Student;
+import com.odas.pojo.User;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -7,7 +9,15 @@ public class MyTest {
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
+    }
+
+    @Test
+    public void test2() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("userBean.xml");
+        // 这里也可以在第二个参数位置填写 User.class 使用反射来定义类的名字
+        User user = context.getBean("user", User.class);
 
     }
+
 }
 

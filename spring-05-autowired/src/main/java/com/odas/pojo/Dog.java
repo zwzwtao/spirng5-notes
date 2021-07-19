1f2bb9893668fc782241bd6636c6d3e04487238c
odas <zwtao106@gmail.com>
2021-07-20 03:03:22 +1000

    autowired 自动装配

---
+++ spring-05-autowired/src/main/java/com/odas/pojo/Dog.java
@@ -0,0 +1,7 @@
+package com.odas.pojo;
+
+public class Dog {
+    public void shout() {
+        System.out.println("wong");
+    }
+}
---
+++ spring-05-autowired/src/main/java/com/odas/pojo/People.java
@@ -0,0 +1,28 @@
+package com.odas.pojo;
+
+public class People {
+    private Cat cat;
+    private Dog dog;
+    private String name;
+
+    public void setCat(Cat cat) {
+        this.cat = cat;
+    }
+
+    public void setDog(Dog dog) {
+        this.dog = dog;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "cat=" + cat +
+                ", dog=" + dog +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}

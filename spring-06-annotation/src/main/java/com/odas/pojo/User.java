51f19884203637830bce4c7bb893f1635f4e57e6
Aiden Tao <zwtao99@outlook.com>
2021-07-22 04:43:06 +1000

    scope 修改

--- spring-06-annotation/src/main/java/com/odas/pojo/User.java
+++ spring-06-annotation/src/main/java/com/odas/pojo/User.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 // id 默认类名小写
 // component 就是组件的意思，使用这个注解，说明这个类被spring管理了(被注册到Spring中)，就是bean
 @Component
-// scope: 作用域，如果是singleton则标识单例，默认应该 prototype
+// scope: 作用域，如果是singleton则标识单例，或者叫 prototype，一般用singleton
 @Scope("prototype")
 public class User {
     // 相当于<property name="name" value="Odas"/>

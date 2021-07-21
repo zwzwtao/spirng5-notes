4214796fc5f1a6bb1b61343c01b25ec6783e957c
Aiden Tao <zwtao99@outlook.com>
2021-07-22 02:22:13 +1000

    autowired 自动装配 注解实现

--- spring-06-annotation/src/main/java/com/odas/pojo/User.java
+++ spring-06-annotation/src/main/java/com/odas/pojo/User.java
@@ -1,12 +1,15 @@
 package com.odas.pojo;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // @Component 等价于 <bean id="user" class="com.odas.pojo.User"/>
 // id 默认类名小写
 // component 就是组件的意思，使用这个注解，说明这个类被spring管理了(被注册到Spring中)，就是bean
 @Component
+// scope: 作用域，如果是singleton则标识单例，默认应该 prototype
+@Scope("prototype")
 public class User {
     // 相当于<property name="name" value="Odas"/>
     @Value("Odas")

40afecf5b56670e6abd91efb1fb50cd525a0244d
Aiden Tao <zwtao99@outlook.com>
2021-08-08 21:38:41 +1000

    MyBatis集成方法

--- spring-10-mybatis/src/test/java/MyTest.java
+++ spring-10-mybatis/src/test/java/MyTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
     @Test
     public void test() {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring-dao.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserMapper userMapper = context.getBean("userMapper", UserMapper.class);
         for (User user : userMapper.selectUser()) {
             System.out.println(user);
